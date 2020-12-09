f9545bf0a11be9319f1c1c0abc332e28459bed9e
weigaihua <weigh@corp.21cn.com>
2020-12-10 00:01:03 +0800

    feat(week03): 提交作业

--- Week03/errgroup.go
+++ Week03/errgroup.go
@@ -1,14 +1,3 @@
-# Week03 作业题目：
-
-基于 errgroup 实现一个 http server 的启动和关闭 ，以及 linux signal 信号的注册和处理，要保证能够一个退出，全部注销退出。
-
-
-
-# 代码解答
-
-
-
-```go
 package main
 
 import (
@@ -28,21 +17,19 @@ func main() {
 	group.Go(func() error {
 		handler := http.NewServeMux()
 		handler.HandleFunc("/err", func(writer http.ResponseWriter, request *http.Request) {
-			// 模拟请求错误
-            servErr <- errors.New("http-req err")
+			servErr <- errors.New("http-req err")
 		})
 		httpServ := &http.Server{
-			Addr:              ":8000",
-			Handler:           handler,
+			Addr:    ":8000",
+			Handler: handler,
 		}
 		go func(s *http.Server) {
-            // 服务请求错误
 			servErr <- httpServ.ListenAndServe()
 		}(httpServ)
 		select {
 		case <-servErr:
 			return errors.New("http server error")
-		case <- egctx.Done()://接收其它goroutine的异常通知
+		case <-egctx.Done():
 			fmt.Println("close the http-server")
 			return httpServ.Close()
 		}
@@ -60,6 +47,3 @@ func main() {
 	err := group.Wait()
 	fmt.Printf("app is over by reason[%v]\n", err)
 }
-
-```
-
